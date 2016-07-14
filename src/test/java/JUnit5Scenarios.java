f0937f203b623457fd4695b31f078f894f6b9825
Piotr Stawirej <piotr.stawirej@gmail.com>
2016-07-14 14:11:05 +0200

    Before annotations.

--- src/test/java/JUnit5Scenarios.java
+++ src/test/java/JUnit5Scenarios.java
@@ -1,12 +1,31 @@
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class JUnit5Scenarios {
 
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("JUnit5Scenarios.beforeAll");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("JUnit5Scenarios.beforeEach");
+    }
+
     @Test
-    void shouldRunTest() {
+    void shouldRunPassingTest() {
         // Then
         assertEquals(3, 1 + 2, "equal");
+    }
+
+    @Test
+    void shouldRunFailingTest() {
+        // Then
+        fail("failing test");
     }
 }
